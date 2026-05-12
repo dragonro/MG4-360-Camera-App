954d7b38c231598a4ae380eb48cc0927732f6f88
jamakr4 <jan.mkrausch@gmail.com>
2026-05-12 11:45:40 +0200

    fix: Button for closing tile view background glitch

--- app/src/main/java/com/drivehub/kamera/OverlayService.java
+++ app/src/main/java/com/drivehub/kamera/OverlayService.java
@@ -232,7 +232,7 @@ public class OverlayService extends Service implements TextureView.SurfaceTextur
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT
         ));
-        //card.setBackgroundResource(R.drawable.bg_overlay_tile);
+        card.setBackgroundResource(R.drawable.bg_overlay_tile);
         card.setClipToOutline(true);
         card.setOutlineProvider(ViewOutlineProvider.BACKGROUND);
 
@@ -254,7 +254,7 @@ public class OverlayService extends Service implements TextureView.SurfaceTextur
         closeParams.leftMargin = dpToPx(3);
         closeParams.topMargin = dpToPx(3);
         btnDismissOverlay.setLayoutParams(closeParams);
-        //btnDismissOverlay.setBackgroundResource(R.drawable.bg_close_button);
+        btnDismissOverlay.setBackgroundResource(R.drawable.bg_close_button);
         btnDismissOverlay.setImageResource(R.drawable.ic_close);
         btnDismissOverlay.setColorFilter(0xFFFFFFFF);
         btnDismissOverlay.setPadding(dpToPx(20), dpToPx(20), dpToPx(20), dpToPx(20));
